ead83aab4f7e4f76b41b10b45b1cc9e5214cf872
Krxwallo <ken.grill@gmx.de>
2021-05-27 20:38:56 +0200

    Probably fixed multiple emotes not working

--- src/main/java/de/embuer/emotes/Listeners/ChatListener.java
+++ src/main/java/de/embuer/emotes/Listeners/ChatListener.java
@@ -7,10 +7,12 @@ import com.velocitypowered.api.event.player.PlayerChatEvent;
 public class ChatListener {
     @Subscribe(order = PostOrder.FIRST)
     public void onPlayerChat(PlayerChatEvent e) {
+        String message = e.getMessage();
         String[] emoteStrings = new String[] {"4Head",  "BRUH",   "D:",     "PauseChamp", "PepeHands", "pepeLaugh", "PepoG",  "PogChamp", "Sadge",  "tf",     "WeirdChamp", "YEP"};
         String[] replacements = new String[] {"\uE001", "\uE002", "\uE003", "\uE004",     "\uE005",    "\uE006",    "\uE007", "\uE008",   "\uE009", "\uE010", "\uE011",     "\uE012"};
         for (int x=0; x<emoteStrings.length; x++) {
-            e.setResult(PlayerChatEvent.ChatResult.message(e.getMessage().replaceAll(emoteStrings[x], replacements[x])));
+            message = e.getMessage().replaceAll(emoteStrings[x], replacements[x]);
         }
+        e.setResult(PlayerChatEvent.ChatResult.message(message));
     }
 }
