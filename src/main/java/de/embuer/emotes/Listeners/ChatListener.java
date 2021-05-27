8d9ad7a7048f425c1d0918ea8351f846d69d1516
Krxwallo <ken.grill@gmx.de>
2021-05-27 20:45:57 +0200

    mooz

--- src/main/java/de/embuer/emotes/Listeners/ChatListener.java
+++ src/main/java/de/embuer/emotes/Listeners/ChatListener.java
@@ -10,7 +10,7 @@ public class ChatListener {
         String message = e.getMessage();
         String[] emoteStrings = new String[] {"4Head",  "BRUH",   "D:",     "PauseChamp", "PepeHands", "pepeLaugh", "PepoG",  "PogChamp", "Sadge",  "tf",     "WeirdChamp", "YEP"};
         String[] replacements = new String[] {"\uE001", "\uE002", "\uE003", "\uE004",     "\uE005",    "\uE006",    "\uE007", "\uE008",   "\uE009", "\uE010", "\uE011",     "\uE012"};
-        for (int x=0; x<emoteStrings.length; x++) {
+        for (int x = 0; x < emoteStrings.length; x++) {
             message = e.getMessage().replaceAll(emoteStrings[x], replacements[x]);
         }
         e.setResult(PlayerChatEvent.ChatResult.message(message));
