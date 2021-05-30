9e7cad112021e8725b7a4d8ef37fbe2d3e926080
mooziii <mooz@moozunity.de>
2021-05-30 11:02:16 +0200

    Implement toml config

---
+++ src/main/java/de/embuer/emotes/listener/ChatListener.java
@@ -0,0 +1,35 @@
+package de.embuer.emotes.listener;
+
+import com.velocitypowered.api.event.PostOrder;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.player.PlayerChatEvent;
+import com.velocitypowered.api.proxy.Player;
+import de.embuer.emotes.HGLaborEmotes;
+import de.embuer.emotes.player.LaborPlayer;
+import de.embuer.emotes.player.PlayerList;
+import net.kyori.adventure.text.Component;
+
+public class ChatListener {
+
+    @Subscribe(order = PostOrder.FIRST)
+    public void onPlayerChat(PlayerChatEvent event) {
+        Player player = event.getPlayer();
+        if (PlayerList.exists(player)) {
+            LaborPlayer laborPlayer = PlayerList.get(player);
+            for (Player it : HGLaborEmotes.server.getAllPlayers()) {
+                Component replace = Component.text(event.getMessage());
+                String message = event.getMessage();
+                for (String emote : HGLaborEmotes.getConfig().getEmotes().keySet()) {
+                    if (message.contains(emote)) {
+                        replace = Component.text(event.getMessage().replaceAll(emote, HGLaborEmotes.getConfig().getEmotes().get(emote)));
+                    }
+                }
+                if (PlayerList.exists(it)) {
+                    it.sendMessage(replace);
+                } else {
+                    it.sendMessage(Component.text(event.getMessage()));
+                }
+            }
+        }
+    }
+}
